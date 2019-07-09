9d5c14f99d3757a696e58823259aa2d615accec5
Bartlomiej Nawoj <thebalowen@gmail.com>
2019-07-09 09:08:52 +0200

    exercises

--- 3_IntroToLists/3_exercises.py
+++ 3_IntroToLists/3_exercises.py
@@ -8,4 +8,11 @@ print("I'd like to invite you, " + invitationList[2].title() + ", to a dinner")
 
 #3-5 Changing guest List
 print(invitationList[0].title() + " can't make it")
-invitationList[0].replace()
+invitationList.remove(invitationList[0])
+invitationList.insert(0,"Kenny")
+print(invitationList)
+print("I'd like to invite you, " + invitationList[0].title() + ", to a dinner")
+print("I'd like to invite you, " + invitationList[1].title() + ", to a dinner")
+print("I'd like to invite you, " + invitationList[2].title() + ", to a dinner")
+#for person in invitationList:
+#   print("I'd like to invite you, " + person + ", to a dinner")
