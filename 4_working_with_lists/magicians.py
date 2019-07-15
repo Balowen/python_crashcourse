5763c13af7b56a0dbabedc7cb51da1dd6ea46166
Bartlomiej Nawoj <thebalowen@gmail.com>
2019-07-15 15:31:13 +0200

    for XD

---
+++ 4_working_with_lists/magicians.py
@@ -0,0 +1,20 @@
+magicians = ['David', 'Brock', 'Alice']
+for magician in magicians:
+    print(magician.title() + ", that was an awesome trick.")
+    print("Can't wait for you next trick, " + magician.title() + ".\n")
+
+#EXERCISES
+print("exercises\n")
+#4.1
+pizzas = ['arabska', 'pepperoni', 'mrozona']
+for pizza in pizzas:
+    print("I like " + pizza + " pizza")
+
+print('DAAAAAMN I love pizza\n')
+
+#4.2
+animals = ['cat', 'dog', 'elephant']
+for animal in animals:
+    print(animal.title() + " would make a great pet")
+print("I would love to get any of those\n")
+
