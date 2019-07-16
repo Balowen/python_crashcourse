6cab36b505dba712a36befd329367c49a4f1d003
Bartlomiej Nawoj <thebalowen@gmail.com>
2019-07-16 11:19:33 +0200

    4_exercises done

--- 4_working_with_lists/4_exercises.py
+++ 4_working_with_lists/4_exercises.py
@@ -6,3 +6,28 @@ for value in range(1,21):
 # million_numbers = list(range(1,1000001))
 # for number in million_numbers:
 #     print(number)
+
+#4-5
+numbers = list(range(1,1000001))
+print("min = " + str(min(numbers))+ "\t\tmax = " + str(max(numbers)))
+print("sum = " + str(sum(numbers)))
+
+#4-6 odd numbers
+odd_numbers = list(range(1,21,2))
+for number in odd_numbers:
+    print(number)
+
+#4-7 threes
+threes = list(range(3,31,3))
+for number in threes:
+    print(number)
+
+#4-8 cubes
+cubes_list = []
+for value in range(1,11):
+    cubes_list.append(value**3)
+print(cubes_list)
+
+#4-9
+cubes_list = [value**3 for value in range(1,11)]
+print(cubes_list)
