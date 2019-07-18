364e093fbb2a1c9f32cbe89ffa2e0ecb16e9ab23
Bartlomiej Nawoj <thebalowen@gmail.com>
2019-07-18 15:11:16 +0200

    some work done

--- 5_if/toppings.py
+++ 5_if/toppings.py
@@ -1,10 +1,22 @@
+available_toppings = ('mushrooms', 'ketchup', 'pineapple',
+                      'onions', 'cheese', 'olives')
+
 requested_toppings = ['mushrooms', 'ketchup',
                       'onions', 'cheese']
+# requested_toppings = []
 
-if requested_toppings != 'anchovies':
-    print("Hold the anchovies!")
-
-if 'cheese' in requested_toppings:
-    print("Adding cheese")
-if 'onions' in requested_toppings:
-    print("Adding extra onions")
+# if requested_toppings != 'anchovies':
+#     print("Hold the anchovies!")
+#
+# if 'cheese' in requested_toppings:
+#     print("Adding cheese")
+# if 'onions' in requested_toppings:
+#     print("Adding extra onions")
+if requested_toppings:
+    for requested_topping in requested_toppings:
+        if requested_topping in available_toppings:
+            print("Adding " + requested_topping + ".")
+        else:
+            print("Sorry, we don't have " + requested_topping + '.')
+else:
+    print("Are you sure you want a plain pizza?")
