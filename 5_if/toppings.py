a692b5c0961f6a7f1ad8ae97006e0a6c6e4f2ce8
Bartlomiej Nawoj <thebalowen@gmail.com>
2019-07-18 13:24:24 +0200

    suma podstawy rowna sie kwadratowi obu ramion

---
+++ 5_if/toppings.py
@@ -0,0 +1,8 @@
+requested_toppings = ['mushrooms', 'ketchup',
+                      'onions', 'cheese']
+
+if requested_toppings != 'anchovies':
+    print("Hold the anchovies!")
+
+if 'cheese' in requested_toppings:
+    print("Damn, that cheese is good")
